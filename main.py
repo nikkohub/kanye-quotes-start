cf3b8863d303d2c38f2318e55723dff0a34cdee5
oliver <revilo@live.se>
2023-02-08 20:56:29 +0100

    Initial commit

---
+++ main.py
@@ -0,0 +1,31 @@
+from tkinter import *
+import requests
+
+
+
+
+def get_quote():
+    mr_kanye = requests.get(url="https://api.kanye.rest")
+    mr_kanye.raise_for_status()
+    kanye_data = mr_kanye.json()["quote"]
+
+    canvas.itemconfig(quote_text, text=kanye_data)
+
+
+window = Tk()
+window.title("Kanye Says...")
+window.config(padx=50, pady=50)
+
+canvas = Canvas(width=300, height=414)
+background_img = PhotoImage(file="background.png")
+canvas.create_image(150, 207, image=background_img)
+quote_text = canvas.create_text(150, 207, text="Kanye Quote Goes HERE", width=250, font=("Arial", 20, "bold"), fill="white")
+canvas.grid(row=0, column=0)
+
+kanye_img = PhotoImage(file="kanye.png")
+kanye_button = Button(image=kanye_img, highlightthickness=0, command=get_quote)
+kanye_button.grid(row=1, column=0)
+
+get_quote()
+
+window.mainloop()
